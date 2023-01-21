7aa7268333d26c932bce68d19982d4e6040fbb8e
jason <jdswart2@svsu.edu>
2023-01-21 12:08:52 -0500

    Bitboards, no moves yet

--- src/main.rs
+++ src/main.rs
@@ -3,59 +3,154 @@ use std::io::{self, Write};
 
 #[derive(Debug, Clone, Copy)]
 enum PieceEnum {
-    King,
-    Queen,
-    Rook,
-    Bishop,
-    Knight,
-    Pawn,
+    BlackKing = 0,
+    BlackQueen,
+    BlackRook,
+    BlackBishop,
+    BlackKnight,
+    BlackPawn,
+    WhiteKing,
+    WhiteQueen,
+    WhiteRook,
+    WhiteBishop,
+    WhiteKnight,
+    WhitePawn,
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Color {
-    Black,
-    White,
-}
-
-#[derive(Debug, Clone, Copy)]
-struct Piece {
-    piece: PieceEnum,
-    color: Color,
-}
-
-impl fmt::Display for Piece {
+impl fmt::Display for PieceEnum {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut piece = match self.piece {
-            PieceEnum::King => "k",
-            PieceEnum::Queen => "q",
-            PieceEnum::Rook => "r",
-            PieceEnum::Bishop => "b",
-            PieceEnum::Knight => "n",
-            PieceEnum::Pawn => "p",
-        };
-
-        let upper = piece.to_ascii_uppercase();
-        match self.color {
-            Color::White => piece = &upper,
-            Color::Black => {}
+        let mut piece = match self {
+            PieceEnum::BlackKing => "k",
+            PieceEnum::BlackQueen => "q",
+            PieceEnum::BlackRook => "r",
+            PieceEnum::BlackBishop => "b",
+            PieceEnum::BlackKnight => "n",
+            PieceEnum::BlackPawn => "p",
+            PieceEnum::WhiteKing => "K",
+            PieceEnum::WhiteQueen => "Q",
+            PieceEnum::WhiteRook => "R",
+            PieceEnum::WhiteBishop => "B",
+            PieceEnum::WhiteKnight => "N",
+            PieceEnum::WhitePawn => "P",
         };
 
         write!(f, "{piece}")
     }
 }
 
-impl Piece {
-    fn new(piece: PieceEnum, color: Color) -> Self {
-        Self { piece, color }
+impl From<usize> for PieceEnum {
+    fn from(item: usize) -> PieceEnum {
+        match item {
+            0 => PieceEnum::BlackKing,
+            1 => PieceEnum::BlackQueen,
+            2 => PieceEnum::BlackRook,
+            3 => PieceEnum::BlackBishop,
+            4 => PieceEnum::BlackKnight,
+            5 => PieceEnum::BlackPawn,
+            6 => PieceEnum::WhiteKing,
+            7 => PieceEnum::WhiteQueen,
+            8 => PieceEnum::WhiteRook,
+            9 => PieceEnum::WhiteBishop,
+            10 => PieceEnum::WhiteKnight,
+            11 => PieceEnum::WhitePawn,
+            _ => unreachable!(),
+        }
     }
 }
 
-#[derive(Debug)]
+type BitBoard = u64;
+
 struct Board {
-    pieces: [Option<Piece>; 64],
+    individual_pieces: [BitBoard; 12],
+
+    black_pieces: BitBoard,
+    white_pieces: BitBoard,
+
+    all_pieces: BitBoard,
 }
 
-impl fmt::Display for Board {
+impl Board {
+    fn starting_position() -> Self {
+        let starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR".to_string();
+        Self::load_fen(starting_fen)
+    }
+
+    fn move_piece(&mut self, from: usize, to: usize) {
+	println!("move {} to {}", from, to);
+    }
+
+    fn load_fen(fen: String) -> Self {
+        let mut individual_pieces = [0; 12];
+        let mut chars = fen.chars().peekable();
+
+        let mut index = 0;
+        while chars.peek().is_some() {
+            let next = chars.next().unwrap();
+            let mut index_increment = 1;
+
+            match next {
+                'k' => individual_pieces[PieceEnum::BlackKing as usize] |= 1 << index,
+                'q' => individual_pieces[PieceEnum::BlackQueen as usize] |= 1 << index,
+                'r' => individual_pieces[PieceEnum::BlackRook as usize] |= 1 << index,
+                'b' => individual_pieces[PieceEnum::BlackBishop as usize] |= 1 << index,
+                'n' => individual_pieces[PieceEnum::BlackKnight as usize] |= 1 << index,
+                'p' => individual_pieces[PieceEnum::BlackPawn as usize] |= 1 << index,
+                'K' => individual_pieces[PieceEnum::WhiteKing as usize] |= 1 << index,
+                'Q' => individual_pieces[PieceEnum::WhiteQueen as usize] |= 1 << index,
+                'R' => individual_pieces[PieceEnum::WhiteRook as usize] |= 1 << index,
+                'B' => individual_pieces[PieceEnum::WhiteBishop as usize] |= 1 << index,
+                'N' => individual_pieces[PieceEnum::WhiteKnight as usize] |= 1 << index,
+                'P' => individual_pieces[PieceEnum::WhitePawn as usize] |= 1 << index,
+                '/' => index_increment = 0,
+                '0'..='8' => index_increment = next as usize - '0' as usize,
+                _ => panic!("Unsupported fen string"),
+            }
+
+            index += index_increment;
+        }
+
+        let mut black_pieces = 0;
+        for piece in individual_pieces.iter().take(6) {
+            black_pieces |= piece;
+        }
+        let mut white_pieces = 0;
+        for piece in individual_pieces.iter().skip(6) {
+            white_pieces |= piece;
+        }
+
+        let all_pieces = black_pieces | white_pieces;
+
+        Board {
+            individual_pieces,
+            black_pieces,
+            white_pieces,
+            all_pieces,
+        }
+    }
+}
+
+struct PrintableBoard {
+    pieces: [Option<PieceEnum>; 64],
+}
+
+impl PrintableBoard {
+    fn new(board: &Board) -> Self {
+        let mut pieces = [None; 64];
+        for piece_idx in (PieceEnum::BlackKing as usize)..=(PieceEnum::WhitePawn as usize) {
+            let piece = board.individual_pieces[piece_idx];
+            for (bit, printable_piece) in pieces.iter_mut().enumerate() {
+                let piece_exists = piece >> bit & 1 == 1;
+                if piece_exists {
+                    *printable_piece = Some(piece_idx.into())
+                }
+            }
+        }
+
+        Self { pieces }
+    }
+}
+
+impl fmt::Display for PrintableBoard {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         writeln!(f, "  +------------+")?;
         for x in 0..8 {
@@ -76,111 +171,6 @@ impl fmt::Display for Board {
     }
 }
 
-impl Board {
-    fn starting_position() -> Self {
-        let starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR".to_string();
-        Self::load_fen(starting_fen)
-    }
-
-    fn move_piece(&mut self, from: usize, to: usize) {
-        let temp = self.pieces[from];
-        if let Some(piece) = temp {
-            match piece.piece {
-                PieceEnum::King => self.move_king(from, to),
-                PieceEnum::Queen => self.move_queen(from, to),
-                PieceEnum::Rook => self.move_rook(from, to),
-                PieceEnum::Bishop => self.move_bishop(from, to),
-                PieceEnum::Knight => self.move_knight(from, to),
-                PieceEnum::Pawn => self.move_pawn(from, to),
-            }
-        }
-    }
-
-    fn move_king(&mut self, _from: usize, _to: usize) {}
-
-    fn move_queen(&mut self, _from: usize, _to: usize) {}
-
-    fn move_rook(&mut self, _from: usize, _to: usize) {}
-
-    fn move_bishop(&mut self, _from: usize, _to: usize) {}
-
-    fn move_knight(&mut self, _from: usize, _to: usize) {}
-
-    fn move_pawn(&mut self, from: usize, to: usize) {
-        self.pieces[to] = self.pieces[from];
-        self.pieces[from] = None;
-    }
-
-    fn load_fen(fen: String) -> Self {
-        let mut pieces = [None; 64];
-        let mut chars = fen.chars().peekable();
-
-        let mut index = 0;
-        while chars.peek().is_some() {
-            let next = chars.next().unwrap();
-
-            match next {
-                'r' => {
-                    pieces[index] = Some(Piece::new(PieceEnum::Rook, Color::Black));
-                    index += 1;
-                }
-                'n' => {
-                    pieces[index] = Some(Piece::new(PieceEnum::Knight, Color::Black));
-                    index += 1;
-                }
-                'b' => {
-                    pieces[index] = Some(Piece::new(PieceEnum::Bishop, Color::Black));
-                    index += 1;
-                }
-                'q' => {
-                    pieces[index] = Some(Piece::new(PieceEnum::Queen, Color::Black));
-                    index += 1;
-                }
-                'k' => {
-                    pieces[index] = Some(Piece::new(PieceEnum::King, Color::Black));
-                    index += 1;
-                }
-                'p' => {
-                    pieces[index] = Some(Piece::new(PieceEnum::Pawn, Color::Black));
-                    index += 1;
-                }
-                'R' => {
-                    pieces[index] = Some(Piece::new(PieceEnum::Rook, Color::White));
-                    index += 1;
-                }
-                'N' => {
-                    pieces[index] = Some(Piece::new(PieceEnum::Knight, Color::White));
-                    index += 1;
-                }
-                'B' => {
-                    pieces[index] = Some(Piece::new(PieceEnum::Bishop, Color::White));
-                    index += 1;
-                }
-                'Q' => {
-                    pieces[index] = Some(Piece::new(PieceEnum::Queen, Color::White));
-                    index += 1;
-                }
-                'K' => {
-                    pieces[index] = Some(Piece::new(PieceEnum::King, Color::White));
-                    index += 1;
-                }
-                'P' => {
-                    pieces[index] = Some(Piece::new(PieceEnum::Pawn, Color::White));
-                    index += 1;
-                }
-                '0'..='8' => {
-                    let i: usize = next as usize - '0' as usize;
-                    index += i;
-                }
-                '/' => {}
-                _ => panic!("Unsupported fen string"),
-            }
-        }
-
-        Board { pieces }
-    }
-}
-
 pub enum Error {
     SamePosition,
     InvalidEntry,
@@ -195,6 +185,7 @@ impl fmt::Display for Error {
     }
 }
 
+// Don't look at this
 pub fn parse_move(mov: &str) -> Result<(usize, usize), Error> {
     let upper = mov.to_ascii_uppercase();
     let mut chars = upper.chars();
@@ -233,7 +224,7 @@ pub fn parse_move(mov: &str) -> Result<(usize, usize), Error> {
 
 fn main() {
     let mut board = Board::starting_position();
-    println!("{board}");
+    println!("{}", PrintableBoard::new(&board));
 
     loop {
         print!("> ");
@@ -259,6 +250,6 @@ fn main() {
         };
 
         board.move_piece(from, to);
-        println!("{board}");
+	println!("{}", PrintableBoard::new(&board));
     }
 }
